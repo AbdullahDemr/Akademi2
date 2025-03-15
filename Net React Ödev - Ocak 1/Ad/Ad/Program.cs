36977914a1b64eaee55c59f61d6c8d2413ebeb45
Abdullah  Demir <Abdullah.demr@hotmail.com>
2025-03-15 13:39:59 +0300

    a
    a

---
+++ Net React Ödev - Ocak 1/Ad/Ad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Adınızı girin: ");
+        string ad = Console.ReadLine();
+
+        Console.WriteLine("Merhaba, " + ad);
+    }
+}
