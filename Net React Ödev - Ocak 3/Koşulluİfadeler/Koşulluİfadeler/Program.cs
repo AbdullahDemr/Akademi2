029e189f4124a3e6415905e81bbf3bd20617b037
Abdullah  Demir <Abdullah.demr@hotmail.com>
2025-03-15 13:31:28 +0300

    a
    a

---
+++ Net React Ödev - Ocak 3/Koşulluİfadeler/Koşulluİfadeler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Yakit miktari girin: ");
+        int yakitSeviyesi = Convert.ToInt32(Console.ReadLine());
+
+        if (yakitSeviyesi < 15)
+        {
+            Console.WriteLine("Yakıttan az kaldı, benzin alın!");
+        }
+        else
+        {
+            Console.WriteLine("Yeterli yakıt var.");
+        }
+
+    }
+}
---
+++ Net React Ödev - Ocak 3/Pozitif,Negatif/Pozitif,Negatif/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi > 0)
+        {
+            Console.WriteLine("Girilen sayı pozitiftir.");
+        }
+        else if (sayi < 0)
+        {
+            Console.WriteLine("Girilen sayı negatiftir.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı sıfırdır.");
+        }
+    }
+}
